b3ab7d314b855c4c1c3a358a92d508c59e5fe970
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-08-07 19:56:06 +0200

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,30 +1,13 @@
-// /* Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item. */
+/*Напиши функцию delay(ms), которая возвращает промис, переходящий в состояние "resolved" через ms миллисекунд.
+Значением исполнившегося промиса должно быть то кол - во миллисекунд которое передали во время вызова функции delay.*/
 
-const categoriesAllEl = document.querySelectorAll('.item').length;
-console.log(`В списке ${categoriesAllEl} категории.`);
+const delay = ms => {
+  return Promise.resolve(ms);
+};
 
-// /* Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега h2)
-// и количество элементов в категории (всех вложенных в него элементов li). */
+const logger = time => console.log(`Resolved after ${time}ms`);
 
-// const categoriesEl = document.querySelectorAll('.item');
-// console.log(
-//   `Категория: ${
-//     categoriesEl[0].querySelector('h2').textContent
-//   }, Количество элементов: ${categoriesEl[0].querySelectorAll('li').length}.`
-// );
-// console.log(
-//   `Категория: ${
-//     categoriesEl[1].querySelector('h2').textContent
-//   }, Количество элементов: ${categoriesEl[1].querySelectorAll('li').length}.`
-// );
-// console.log(
-//   `Категория: ${
-//     categoriesEl[2].querySelector('h2').textContent
-//   }, Количество элементов: ${categoriesEl[2].querySelectorAll('li').length}.`
-// );
-
-const categoriesEl = document.querySelectorAll('.item');
-categoriesEl.forEach(category => {
-  console.log('Категория:', category.querySelector('h2').textContent);
-  console.log('Количество элементов:', category.querySelectorAll('li').length);
-});
+// Вызовы функции для проверки
+delay(2000).then(logger); // Resolved after 2000ms
+delay(1000).then(logger); // Resolved after 1000ms
+delay(1500).then(logger); // Resolved after 1500ms
